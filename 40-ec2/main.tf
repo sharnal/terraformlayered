4ce0a99451b9c9ab63090b03852937d793d67747
lakshmikan <lakshmikanthareddy001@gmail.com>
2025-03-22 09:00:02 +0000

    getting subnet from state file

--- 40-ec2/main.tf
+++ 40-ec2/main.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "example-lk" {
+resource "aws_instance" "example-lakshmikan" {
 	ami = "ami-00bb6a80f01f03502"
 	instance_type ="t3.micro"
-	subnet_id = "subnet-0bfffa2f78071fc48"
+	subnet_id = data.terraform_remote_state._30-subnet.outputs.subnet-shubham_id
 
 tags = {
     name = "ec2-lakshmikan-${var.environment}"
